7498493231e2724e16ca0047a0c2177a0c2e016a
gstark0 <gstark0@icloud.com>
2019-01-12 13:16:38 +0100

    Cart checkout

--- cart.py
+++ cart.py
@@ -7,7 +7,20 @@ db_name = 'database.db'
 
 cart_api = Blueprint('cart_api', __name__)
 
-# Add product to cart
+# Complete the cart
+@cart_api.route('/api/cart/<int:cart_id>/complete', methods=['POST'])
+def complete(cart_id):
+	with sqlite3.connect(db_name) as conn:
+		cur = conn.cursor()
+
+		# Update product's inventory_count
+		cur.execute('''UPDATE products
+						SET inventory_count = inventory_count - (SELECT quantity FROM cart_items WHERE cart_id = ? AND product_id = products.product_id)
+						WHERE product_id = (SELECT product_id FROM cart_items WHERE cart_id = ? AND product_id = products.product_id)''', (cart_id, cart_id))
+
+	return jsonify('cart %s completed' % cart_id)
+
+# Add product to the cart
 @cart_api.route('/api/cart/<int:cart_id>', methods=['GET', 'POST'])
 def cart(cart_id):
 
@@ -49,10 +62,9 @@ def cart(cart_id):
 				# Add an item to the cart
 				cur.execute('INSERT INTO cart_items (cart_id, product_id, quantity) VALUES (?, ?, ?)', (cart_id, product_id, quantity))
 
-		elif request.method == 'GET':
-			# Query cart by id and calculate total prices
-			cur.execute('SELECT products.product_id, products.title, products.price, round(products.price * cart_items.quantity, 2) AS total, cart_items.quantity FROM products JOIN cart_items USING (product_id)')
-			out = cur.fetchall()
-			total = round(sum(d['total'] for d in out), 2)
+		# Query cart by id and calculate total prices
+		cur.execute('SELECT products.product_id, products.title, products.price, round(products.price * cart_items.quantity, 2) AS total, cart_items.quantity FROM products JOIN cart_items USING (product_id)')
+		out = cur.fetchall()
+		total = round(sum(d['total'] for d in out), 2)
 
 	return jsonify({'total': total, 'products': out})
